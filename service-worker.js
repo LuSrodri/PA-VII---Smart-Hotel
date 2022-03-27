b6c63dfeace1490f23f3c0f456b13e74a298b019
LuSrodri <lucassrodri08@gmail.com>
2022-03-27 01:17:48 -0300

    definindo o manifest.webmanifest e o service-worker

--- service-worker.js
+++ service-worker.js
@@ -28,9 +28,8 @@ self.addEventListener("install", (event) => {
             // Setting {cache: 'reload'} in the new request will ensure that the
             // response isn't fulfilled from the HTTP cache; i.e., it will be from
             // the network.
-            await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
             await cache.addAll([
-                '/offline',
+                new Request(OFFLINE_URL, { cache: "reload" }),
                 '/styles.css',
                 '/scripts.js',
                 '/logo.png',
