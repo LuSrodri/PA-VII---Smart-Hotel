74c8a6b58fa2c98b2bba52a1e701dcd636c2ed6e
LuSrodri <lucassrodri08@gmail.com>
2022-03-27 00:15:24 -0300

    definindo o manifest.webmanifest e o service-worker

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/offline', (req, res) => {
+  app.use(express.static(__dirname))
+  res.sendFile(__dirname + '/offline.html');
+})
+
 app.listen(port, () => {
   //console.log(`Example app listening on port ${port}`)
 })
